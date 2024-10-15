69d63f3bbfb513ffe0334979fd9385b83f5adb2a
Avenel Théophile <theophile.avenel@gmail.com>
2024-10-15 23:15:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 import ee
+import json
+import os
+import google.oauth2.credentials
 
-# Initialize the Earth Engine API
-ee.Initialize()
+# Load credentials from the environment variable
+stored = json.loads(os.getenv("EARTHENGINE_TOKEN"))
+credentials = google.oauth2.credentials.Credentials(
+    None,
+    token_uri="https://oauth2.googleapis.com/token",
+    client_id=stored["client_id"],
+    client_secret=stored["client_secret"],
+    refresh_token=stored["refresh_token"],
+    quota_project_id=stored["project"],
+)
+
+# Initialize the Earth Engine API with the credentials
+ee.Initialize(credentials=credentials)
+
+# Print a greeting message from the Earth Engine servers
+print(ee.String("Greetings from the Earth Engine servers!").getInfo())
 
 # Test the API: Print the elevation of Mount Everest
 dem = ee.Image('USGS/SRTMGL1_003')
